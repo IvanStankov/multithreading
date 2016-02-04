9211f02d9572569a83f671980ecf253369353538
IvanStankov <ivan.stankov1992@gmail.com>
2016-02-05 00:36:54 +0600

    Bus beginning

---
+++ src/main/java/com/ivan/jmp/bus/Consumer.java
@@ -0,0 +1,7 @@
+package com.ivan.jmp.bus;
+
+/**
+ * Created by Иван on 05.02.2016.
+ */
+public class Consumer {
+}
---
+++ src/main/java/com/ivan/jmp/bus/Queue.java
@@ -0,0 +1,8 @@
+package com.ivan.jmp.bus;
+
+/**
+ * Created by Иван on 05.02.2016.
+ */
+public class Queue {
+
+}
